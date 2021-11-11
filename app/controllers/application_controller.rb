08f9a8a6aabba2749dcea6866d7c34ffec7b213b
sebi-golfgenius <sebastian.pomirleanu@golfgenius.com>
2021-11-11 13:40:38 +0200

    Finish static pages. Add root to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "Hello, sample-app"
-  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "RoR Tutorial Sample App"
   end
 
+  test "shoudl get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
