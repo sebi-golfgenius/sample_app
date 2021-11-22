b8ac4752d1b4690877a980f83ec051970435fc84
sebi-golfgenius <sebastian.pomirleanu@golfgenius.com>
2021-11-22 19:07:37 +0200

    Add test for checking the home feed and a commet with a line that I couldn't make work in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -92,6 +92,10 @@ class User < ApplicationRecord
     following_ids = "SELECT followed_id FROM relationships
                      WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id) # following_ids - provided by ActiveRecord. Returns a list with all ids of followed users
+
+    # other option
+    # part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    # Micropost.joins(user: :followers).where(part_of_feed, { id: id })
   end
 
   # Follow a user
